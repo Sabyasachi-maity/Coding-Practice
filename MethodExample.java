3f77bdf0de98f8140a6fc6a0873c8f1270ca2c48
Sabyasachi Maity <sabyasachimaity2000@gmail.com>
2024-06-07 23:57:12 +0530

    Passing Parameters

---
+++ MethodExample.java
@@ -0,0 +1,10 @@
+public class MethodExample {
+
+    static void Details(String name) {
+        System.out.println("Welcome " + name);
+    }
+    
+    public static void main(String[] args) {
+        Details("Mitali");
+    }
+}
