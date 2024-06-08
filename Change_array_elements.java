b6c024993c428607e91696aa3bf74a2eadd25fd1
Sabyasachi Maity <sabyasachimaity2000@gmail.com>
2024-06-08 23:56:37 +0530

    arrays

---
+++ Change_array_elements.java
@@ -0,0 +1,9 @@
+public class Change_array_elements {
+    public static void main(String[] args) {
+        //creating array objects
+        String[] cities = {"Delhi", "Mumbai", "Lucknow", "Pune", "Chennai"};
+
+        cities[2] = "Bangalore";
+        System.out.println(cities[2]);
+    }
+}
---
+++ Length_of_an_array.java
@@ -0,0 +1,10 @@
+public class Length_of_an_array {
+    public static void main(String[] args) {
+        //creating array objects
+        String[] cities = {"Delhi", "Mumbai", "Lucknow", "Pune", "Chennai"};
+        int[] numbers = {25,93,48,95,74,63,87,11,36};
+
+        System.out.println("Number of Cities: " + cities.length);
+        System.out.println("Length of Num List: " + numbers.length);
+    }
+}
