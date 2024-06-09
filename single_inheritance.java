edf319c34b2e39e15a4843fc9e1cba7b1cad0792
Sabyasachi Maity <sabyasachimaity2000@gmail.com>
2024-06-09 23:53:03 +0530

    Inheritance

---
+++ single_inheritance.java
@@ -0,0 +1,19 @@
+class single_inheritance {
+    void Force() {
+        System.out.println("There are four fundamental forces.");
+    }
+}
+
+class Gravitational extends single_inheritance {
+    void Gravity() {
+        System.out.println("Fruits fall to the ground due to gravitational Force.");
+    }
+}
+
+class SingleInheritance {
+    public static void main(String[] args) {
+        Gravitational G = new Gravitational();
+        G.Force();
+        G.Gravity();
+    }
+}
